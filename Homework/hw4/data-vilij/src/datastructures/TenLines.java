da4cbdad6e19029cbec50943b53db48aa3de1952
Niko_Ziozis <nikolaos.ziozis@stonybrook.edu>
2018-04-03 17:15:51 -0400

    Updates the the TenLines data structure. Added in two constructors and changed around how the data will be passed between totalData, passiveData, and activeData

--- Homework/hw4/data-vilij/src/datastructures/TenLines.java
+++ Homework/hw4/data-vilij/src/datastructures/TenLines.java
@@ -3,8 +3,24 @@ package datastructures;
 import java.util.ArrayList;
 
 public class TenLines<T> {
-    private ArrayList<T> activeArea = new ArrayList<>();
-    private ArrayList<T> passiveArea = new ArrayList<>();
+    private ArrayList<T> activeArea;
+    private ArrayList<T> passiveArea;
+    private ArrayList<T> totalData;
+
+
+    public TenLines() {
+        activeArea = new ArrayList<>();
+        passiveArea = new ArrayList<>();
+        totalData = new ArrayList<>();
+    }
+
+    public TenLines(ArrayList<T> totalData) {
+        activeArea = new ArrayList<>();
+        passiveArea = totalData;
+        this.totalData = totalData;
+        update_activeArea();
+    }
+
 
     public ArrayList<T> get_activeArea() {
         return activeArea;
@@ -15,18 +31,27 @@ public class TenLines<T> {
     }
 
     public ArrayList<T> get_totalData() {
-        ArrayList<T> totalData = new ArrayList<>();
-        totalData.addAll(activeArea);
-        totalData.addAll(passiveArea);
         return totalData;
     }
 
     /**
+     * this should only be called when new data is being loaded in. This completely resets the data that the
+     * application is working with to the initial case. This should also only really be needed to be called when the
+     * default constructor is used as this is already handled in the other version.
+     */
+    public void setTotalData(ArrayList<T> totalData) {
+        this.totalData = totalData;
+        passiveArea = totalData;
+        update_activeArea();
+    }
+
+
+    /**
      * This will take elements from the passiveArea until it reaches a size of 10.
      */
-    public void update_activeArea() {
+    private void update_activeArea() {
         int index = 0;
-        while (activeArea.size() < 10) {
+        while (activeArea.size() < 10 || passiveArea.size() == 0) {
             activeArea.add(passiveArea.remove(index));
             index++;
         }
@@ -36,8 +61,8 @@ public class TenLines<T> {
      * All of the data loaded in will initially go into the passiveArea. It is an ArrayList because the order matters
      */
     //TODO Figure out how you want to load the data from the file into this ArrayList. How will load() and TSD prompt work for this
-    public void update_passiveArea() {
+    public void update_passiveArea(T data) {
+        passiveArea.add(data);
     }
-
 
 }
