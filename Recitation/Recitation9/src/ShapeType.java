8619655732367aa8f46ad679bc988df0f475042d
Niko_Ziozis <nikolaos.ziozis@stonybrook.edu>
2018-04-09 09:03:53 -0400

    Added the unversioned enum ShapeType.

---
+++ Recitation/Recitation9/src/ShapeType.java
@@ -0,0 +1,17 @@
+
+public enum ShapeType{;
+
+    public class Rectangle{
+        Rectangle rectangle = new Rectangle();
+    }
+
+    public class Circle{
+        Circle circle = new Circle();
+    }
+
+    public class Square{
+        Square square = new Square();
+    }
+
+
+}
