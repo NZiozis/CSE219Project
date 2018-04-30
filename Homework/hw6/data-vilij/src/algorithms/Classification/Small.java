6f27938e5a9ed58cf6b650210b587ea781b3e37b
Niko_Ziozis <nikolaos.ziozis@stonybrook.edu>
2018-04-30 10:36:22 -0400

    This is the beginning of hw6, the last hw

---
+++ Homework/hw6/data-vilij/src/algorithms/Classification/Small.java
@@ -0,0 +1,26 @@
+package algorithms.Classification;
+
+import algorithms.Classifier;
+import algorithms.DataSet;
+import datastructures.Drop;
+
+public class Small extends Classifier{
+
+    public Small(DataSet dataset, Drop drop, int maxIterations, int updateInterval, boolean toContinue){}
+
+    @Override
+    public int getMaxIterations(){
+        return 0;
+    }
+
+    @Override
+    public int getUpdateInterval(){
+        return 0;
+    }
+
+    @Override
+    public boolean tocontinue(){
+        return false;
+    }
+
+}
---
+++ Homework/hw6/data-vilij/src/datastructures/ConfigurationDialog.java
@@ -0,0 +1,181 @@
+package datastructures;
+
+import actions.AppActions;
+import dataprocessors.AppData;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import settings.AppPropertyTypes;
+import ui.AppUI;
+import vilij.components.Dialog;
+import vilij.templates.ApplicationTemplate;
+
+import java.util.ArrayList;
+
+public class ConfigurationDialog extends Stage implements Dialog{
+
+    private ApplicationTemplate applicationTemplate;
+    private TextArea            maxIterations;
+    private TextArea            updateInterval;
+    private CheckBox            continuousRun;
+    private boolean             isClustering;
+    private ComboBox<String>    numLables;
+    private String              numberOfLabelsChosen;
+    private Integer             row;
+    private Integer             chosenNumberOfLabels;
+
+    public ConfigurationDialog(ApplicationTemplate applicationTemplate, boolean isClustering, Integer row){
+        this.applicationTemplate = applicationTemplate;
+        this.isClustering = isClustering;
+        this.row = row;
+    }
+
+    @Override
+    public void show(String dialogTitle, String message){
+        setTitle(dialogTitle);           // set the title of the dialog
+        showAndWait();                   // open the dialog and wait for the user to click the close button
+    }
+
+    private String invalidInputHandler(String string){
+        try{
+            int fault = Integer.parseInt(string);
+            if (fault < 1){
+                throw new Exception();
+            }
+            return string;
+        }
+        catch (Exception e){
+            return "1";
+        }
+    }
+
+    @Override
+    public void init(Stage owner){
+
+        initModality(Modality.WINDOW_MODAL);
+        initOwner(owner);
+
+        Button confirmationButton = new Button(Option.CONFIRM.name());
+        int rowIndex = 0;
+
+        HBox buttonBox = new HBox(5);
+        buttonBox.getChildren().add(confirmationButton);
+
+        GridPane settingsPane = new GridPane();
+        settingsPane.setHgap(10);
+        settingsPane.setVgap(10);
+        settingsPane.setPadding(new Insets(10, 10, 10, 10));
+
+        settingsPane.add(new Text(
+                                 applicationTemplate.manager.getPropertyValue(AppPropertyTypes.MAX_ITERATIONS_TEXT.name())), 0,
+                         rowIndex);
+        maxIterations = new TextArea();
+        maxIterations.setMaxSize(100, 30);
+        maxIterations.setMinSize(100, 30);
+        settingsPane.add(maxIterations, 1, rowIndex++);
+
+        settingsPane.add(new Text(
+                                 applicationTemplate.manager.getPropertyValue(AppPropertyTypes.UPDATE_INTERVAL_TEXT.name())), 0,
+                         rowIndex);
+        updateInterval = new TextArea();
+        updateInterval.setMaxSize(100, 30);
+        updateInterval.setMinSize(100, 30);
+        settingsPane.add(updateInterval, 1, rowIndex++);
+
+        if (isClustering){
+            settingsPane
+                    .add(new Text(applicationTemplate.manager.getPropertyValue(AppPropertyTypes.LABELS_NUM.name())), 0,
+                         rowIndex);
+            numLables = new ComboBox<>();
+            for (int i = 0; i < ( (AppData) applicationTemplate.getDataComponent() ).getLabels().size(); i++){
+                numLables.getItems().add(i + 1 + "");
+            }
+            settingsPane.add(numLables, 1, rowIndex++);
+        }
+
+        settingsPane.add(new Text(
+                                 applicationTemplate.manager.getPropertyValue(AppPropertyTypes.CONTINUOUS_RUN_TEXT.name())), 0,
+                         rowIndex);
+        continuousRun = new CheckBox();
+        settingsPane.add(continuousRun, 1, rowIndex);
+
+        VBox messagePane = new VBox(settingsPane, buttonBox);
+        messagePane.setAlignment(Pos.CENTER);
+        messagePane.setPadding(new Insets(10, 20, 20, 20));
+        messagePane.setSpacing(10);
+
+        confirmationButton.setOnMouseClicked(event -> {
+
+            ( (AppActions) applicationTemplate.getActionComponent() ).setFirstIteration(true);
+
+            maxIterations.setText(invalidInputHandler(maxIterations.getText()));
+            updateInterval.setText(invalidInputHandler(updateInterval.getText()));
+
+            if (Integer.parseInt(updateInterval.getText()) > Integer.parseInt(maxIterations.getText())){
+                updateInterval.setText(maxIterations.getText());
+            }
+
+            if (isClustering){
+                numberOfLabelsChosen = invalidInputHandler(numLables.getValue());
+                numLables.setValue(numberOfLabelsChosen);
+            }
+
+            continuousRun.setSelected(continuousRun.isSelected());
+            if (this.row.equals(( (AppUI) applicationTemplate.getUIComponent() ).getCurrentAlgoIndex())){
+                ( (AppUI) applicationTemplate.getUIComponent() ).setConfigurationValid(true);
+            }
+
+            ArrayList<Integer> externalConfig =
+                    ( (AppData) applicationTemplate.getDataComponent() ).getCurrentAlgorithmConfiguration();
+
+            externalConfig.clear();
+
+            externalConfig.add(Integer.parseInt(maxIterations.getText()));
+            externalConfig.add(Integer.parseInt(updateInterval.getText()));
+            if (isClustering) externalConfig.add(Integer.parseInt(numLables.getValue()));
+
+            /*Here, 1 means that it is selected and 0 means that it isn't*/
+            if (continuousRun.isSelected()){ externalConfig.add(1); }
+            else{ externalConfig.add(0); }
+
+            this.close();
+        });
+
+        this.setScene(new Scene(messagePane));
+    }
+
+    @SuppressWarnings("unused")
+    public CheckBox getContinuousRun(){
+        return continuousRun;
+    }
+
+    @SuppressWarnings("unused")
+    public TextArea getMaxIterations(){
+        return maxIterations;
+    }
+
+    @SuppressWarnings("unused")
+    public TextArea getUpdateInterval(){
+        return updateInterval;
+    }
+
+    public enum Option{
+        CONFIRM("Confirm");
+
+
+        @SuppressWarnings("unused")
+        private String option;
+
+        Option(String option){this.option = option;}
+    }
+}
